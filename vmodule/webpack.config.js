4c2af68fea2b8fa634cf073c80ce0a15ff0521bd
Cristian Echeverría <cer1969@gmail.com>
2016-06-01 00:14:28 -0400

    Configuraciones para generar m?dulos web, node o umd (mixtos)

--- vmodule/webpack.config.js
+++ vmodule/webpack.config.js
@@ -2,11 +2,15 @@ const webpack = require('webpack');
 
 module.exports = {
     entry: './src/cx.ts',
+    target: "web",
+    //target: "node", // for Node.js excluyendo node import
     output: {
         path: './out',
         filename: 'cx.min.js',
         library: 'cx',
-        libraryTarget: "commonjs2"
+        //libraryTarget: "var" // Browser
+        //libraryTarget: "commonjs2" // Node.js
+        libraryTarget: "umd" // Browser and Node.js
     },
     resolve: {
         extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
