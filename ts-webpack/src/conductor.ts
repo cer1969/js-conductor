898b47e0333521db448f2ebc87f6fbfb48cb88f6
Cristian Echeverr?a <cer1969@gmail.com>
2016-10-27 09:56:44 -0300

    Tabs

--- ts-webpack/src/conductor.ts
+++ ts-webpack/src/conductor.ts
@@ -5,19 +5,19 @@ import {Category} from "./category"
 //--------------------------------------------------------------------------------------------------
 
 export class Conductor {
-    /*
-    Container for conductor characteristics
-    name     : Name of conductor
-    category : Category instance
-    diameter : Diameter [mm]
-    area     : Cross section area [mm2]
-    weight   : Weight per unit [kg/m]
-    strength : Rated strength [kg]
-    r25      : Resistance at 25°C [Ohm/km]
-    hcap     : Heat capacity [kcal/(ft*°C)]
-    idx      : Database key
-    */
-    constructor(public name: string, public category: Category, public diameter: number, 
-                public area: number, public weight: number, public strength: number,
-                public r25: number, public hcap: number, public idx?: string) {}
+	/*
+	Container for conductor characteristics
+	name.... : Name of conductor
+	category : Category instance
+	diameter : Diameter [mm]
+	area..   : Cross section area [mm2]
+	weight   : Weight per unit [kg/m]
+	strength : Rated strength [kg]
+	r25.     : Resistance at 25°C [Ohm/km]
+	hcap..   : Heat capacity [kcal/(ft*°C)]
+	idx.     : Database key
+	*/
+	constructor(public name: string, public category: Category, public diameter: number,
+	            public area: number, public weight: number, public strength: number,
+	            public r25: number, public hcap: number, public idx?: string) {}
 }
--- ts-webpack/src/operatingtable.ts
+++ ts-webpack/src/operatingtable.ts
@@ -7,98 +7,98 @@ import * as k from "./constants"
 //--------------------------------------------------------------------------------------------------
 
 export class OperatingItem {
-    /*
-    Container for conductor and operating conditions
+	/*
+	Container for conductor and operating conditions
 
-    Read-only properties
-    currentcalc : CurrentCalc instance
-    tempMaxOp   : Maximux operating temperature for currentcalc.conductor [°C]
-    nsc         : Number of subconductor per fase
-    */
-    private _currentcalc: CurrentCalc;
-    private _tempMaxOp: number;
-    private _nsc: number;
+	Read-only properties
+	currentcalc : CurrentCalc instance
+	tempMaxOp   : Maximux operating temperature for currentcalc.conductor [°C]
+	nsc         : Number of subconductor per fase
+	*/
+	private _currentcalc: CurrentCalc;
+	private _tempMaxOp: number;
+	private _nsc: number;
 
-    constructor(currentcalc: CurrentCalc, tempMaxOp=50.0, nsc=1, altitude=300.0, emissivity=0.5) {
-        /*
-        currentcalc : CurrentCalc instance
-        tempMaxOp   : Maximum operating temperature for currentcalc.conductor [°C]
-        nsc         : Number of subconductor per face
-        altitude    : Altitude [m] = 300.0
-        emissivity  : Emissivity (0 to 1) = 0.5
-        */
-        currentcalc.altitude = altitude;
-        currentcalc.emissivity = emissivity;
-        check(tempMaxOp).ge(k.TC_MIN).le(k.TC_MAX);
-        check(nsc).ge(1);
+	constructor(currentcalc: CurrentCalc, tempMaxOp=50.0, nsc=1, altitude=300.0, emissivity=0.5) {
+		/*
+		currentcalc : CurrentCalc instance
+		tempMaxOp   : Maximum operating temperature for currentcalc.conductor [°C]
+		nsc         : Number of subconductor per face
+		altitude    : Altitude [m] = 300.0
+		emissivity  : Emissivity (0 to 1) = 0.5
+		*/
+		currentcalc.altitude = altitude;
+		currentcalc.emissivity = emissivity;
+		check(tempMaxOp).ge(k.TC_MIN).le(k.TC_MAX);
+		check(nsc).ge(1);
 
-        this._currentcalc = currentcalc;
-        this._tempMaxOp = tempMaxOp;
-        this._nsc = nsc;
-    }
+		this._currentcalc = currentcalc;
+		this._tempMaxOp = tempMaxOp;
+		this._nsc = nsc;
+	}
 
-    // Public methods
+	// Public methods
 
-    getCurrent(ta: number): number {
-        /*
-        Returns current for the OperatingItems [ampere]
-        ta : Ambient temperature [°C]
-        */
-        return this._currentcalc.getCurrent(ta, this._tempMaxOp) * this._nsc
-    }
+	getCurrent(ta: number): number {
+		/*
+		Returns current for the OperatingItems [ampere]
+		ta : Ambient temperature [°C]
+		*/
+		return this._currentcalc.getCurrent(ta, this._tempMaxOp) * this._nsc
+	}
 
-    getCurrentList(taList: Array<number>): Array<number> {
-        /*
-        Returns list with current [ampere]
-        taList: Secuence with ambient temperatures [°C]
-        */
-        return taList.map(x => this.getCurrent(x));
-    }
+	getCurrentList(taList: Array<number>): Array<number> {
+		/*
+		Returns list with current [ampere]
+		taList: Secuence with ambient temperatures [°C]
+		*/
+		return taList.map(x => this.getCurrent(x));
+	}
 
-    // Propiedades
+	// Propiedades
 
-    get currentcalc(): CurrentCalc {
-        return this._currentcalc;
-    }
+	get currentcalc(): CurrentCalc {
+		return this._currentcalc;
+	}
 
-    set currentcalc(value: CurrentCalc) {
-    	throw new RangeError('OperatingItem.CurrentCalc is readonly');
-    }
+	set currentcalc(value: CurrentCalc) {
+		throw new RangeError('OperatingItem.CurrentCalc is readonly');
+	}
 
-    get tempMaxOp(): number {
-        return this._tempMaxOp;
-    }
+	get tempMaxOp(): number {
+		return this._tempMaxOp;
+	}
 
-    set tempMaxOp(value: number) {
-    	throw new RangeError('OperatingItem.tempMaxOp is readonly');
-    }
+	set tempMaxOp(value: number) {
+		throw new RangeError('OperatingItem.tempMaxOp is readonly');
+	}
 
-    get nsc(): number {
-        return this._nsc;
-    }
+	get nsc(): number {
+		return this._nsc;
+	}
 
-    set nsc(value: number) {
-        throw new RangeError('OperatingItem.nsc is readonly');
-    }
+	set nsc(value: number) {
+		throw new RangeError('OperatingItem.nsc is readonly');
+	}
 }
 
 export class OperatingTable {
-    /*
-    Mutable secuence to store OperatingItem instances and calculates current.
+	/*
+	Mutable secuence to store OperatingItem instances and calculates current.
 
-    Constructor (Read-write properties)
-    items : Secuence with OperatingItem instance
-    idx   : Database key
-    */
-    constructor(public items: Array<OperatingItem> = [], public idx?: string) {}
+	Constructor (Read-write properties)
+	items : Secuence with OperatingItem instance
+	idx   : Database key
+	*/
+	constructor(public items: Array<OperatingItem> = [], public idx?: string) {}
 
-    getCurrent(ta: number): number {
-        /*
-        Returns lowest current for the OperatingItems contained [ampere]
-        ta : Ambient temperature [°C]
-        */
-        let ampList = this.items.map(item => item.getCurrent(ta));
-        return Math.min(...ampList);
-    }
+	getCurrent(ta: number): number {
+		/*
+		Returns lowest current for the OperatingItems contained [ampere]
+		ta : Ambient temperature [°C]
+		*/
+		let ampList = this.items.map(item => item.getCurrent(ta));
+		return Math.min(...ampList);
+	}
 
 }
